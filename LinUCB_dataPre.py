85e5a96529888e48e4208c7bb9919917129b735a
oskarryn <oskar.rynkiewicz@gmail.com>
2019-11-01 16:31:08 +0100

    changes to make it easy to pass different number of features than K=30 for factorization. Changed data path to project dir . instead of ..

--- LinUCB_dataPre.py
+++ LinUCB_dataPre.py
@@ -23,32 +23,37 @@ MovieLens dataset prepared for LinUCB
 # Data Preperation
 class MovieLensData:
 
-    def __init__(self):
+    def __init__(self, d=30):
+        '''d <int> is effectively the number of features'''
         self.n_users = 0
         self.n_movies = 0
         
         self.read_data()
         self.topN_movies_users()
-        U, Vt, M = self.matrix_factorization()
+        U, Vt, M = self.matrix_factorization(K=d)
         
         self.U = U
         self.Vt = Vt
         self.M = M / 5.
         
     def read_data(self):
-        self.ratings = pd.read_table('../ml-1m/ratings.dat', sep='::', 
-                                names = ['UserID', 'MovieID', 'Rating', 'Timestamp'],
-                                encoding = 'latin1',
-                                engine = 'python')
-        self.movies  = pd.read_table('../ml-1m/movies.dat',  sep='::',
-                                names = ['MovieID', 'Title', 'Genres'], 
-                                encoding = 'latin1',
-                                engine ='python')
-        self.users   = pd.read_table('../ml-1m/users.dat',  sep='::', 
-                                names = ['UserID', 'Gender', 'Age', 'Occupation', 'Zip'], 
-                                encoding = 'latin1',
-                                engine = 'python')
-    
+        try:
+            self.ratings = pd.read_table('./ml-1m/ratings.dat', sep='::', 
+                                    names = ['UserID', 'MovieID', 'Rating', 'Timestamp'],
+                                    encoding = 'latin1',
+                                    engine = 'python')
+            self.movies  = pd.read_table('./ml-1m/movies.dat',  sep='::',
+                                    names = ['MovieID', 'Title', 'Genres'], 
+                                    encoding = 'latin1',
+                                    engine ='python')
+            self.users   = pd.read_table('./ml-1m/users.dat',  sep='::', 
+                                    names = ['UserID', 'Gender', 'Age', 'Occupation', 'Zip'], 
+                                    encoding = 'latin1',
+                                    engine = 'python')
+        except FileNotFoundError:
+            print('Directory ml-1m not found. Download http://files.grouplens.org/datasets/movielens/ml-1m.zip and extract it to the working directory')
+            raise
+
     def topN_movies_users(self, N = 1000):
         ratings_count = self.ratings.groupby(by='MovieID', as_index=True).size()
         top_ratings = ratings_count[ratings_count>=N]
@@ -75,3 +80,8 @@ class MovieLensData:
 
     def active_users(self):
         return np.argwhere((self.M != 0).sum(1) > 150).flatten()
+
+#utility
+def sparsity(arr):
+        assert(arr.ndim==2)
+        return round(1.0 - (np.count_nonzero(arr) / (arr.shape[0]*arr.shape[1])), 3)
